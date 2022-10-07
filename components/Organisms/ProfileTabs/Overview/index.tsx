b4336a00f43a63800c10aeddab3dc1169852f51b
joycecoding <joyalonge01@gmail.com>
2022-10-07 03:06:09 -0700

    overview fixes

--- components/Organisms/ProfileTabs/Overview/index.tsx
+++ components/Organisms/ProfileTabs/Overview/index.tsx
@@ -1,26 +1,74 @@
 const Overview = () => {
-  return <div className="bg-white px-5 py-5 rounded-lg">
-    <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates reprehenderit pariatur, consequuntur saepe non animi sed doloribus alias! Perspiciatis aperiam doloribus voluptate eligendi, vero excepturi iusto maxime velit cum amet.
-    Neque consectetur laboriosam aut, ipsa totam magni ut possimus sunt laborum optio, pariatur similique atque aliquam sapiente dolorum fugit expedita eius accusantium porro non nam quod doloremque rem! Voluptate, veniam?
-    Deleniti, ut animi harum facere voluptate laudantium. Quo similique nihil quis illum tempora perferendis, consectetur, ullam at deleniti vero deserunt omnis pariatur, dolore laboriosam laudantium modi a sed harum autem?
-    Eligendi nisi autem error quidem assumenda voluptatibus fugiat aliquam ad commodi suscipit tempore a obcaecati cupiditate maiores, iure facere quod accusantium laudantium ut et quisquam magni omnis pariatur. Harum, aliquam.
-    Nesciunt quo repellat numquam asperiores voluptatibus similique laboriosam vitae, excepturi dolorum tempora nulla deleniti aut sequi maxime atque cum dolorem quae doloremque tempore ut, officiis illum velit labore. Sequi, consequatur.
-    Velit voluptates, at assumenda magnam, enim ad ullam quasi architecto quis, deleniti odio ipsum fugit voluptatibus hic voluptate magni. Ipsam suscipit dolorem, maxime fugiat quas itaque cum a excepturi omnis.
-    Error quam consequuntur repudiandae provident deserunt, voluptatem harum earum alias cum facere autem quidem maiores cupiditate rem praesentium aliquam porro? Et, eum inventore? Repudiandae at repellendus totam! Odio, at dolore!
-    Quidem dicta earum officia, laboriosam quis animi facilis illum? Perspiciatis aperiam voluptas, reiciendis amet officia fugiat id temporibus sunt sed modi veritatis aspernatur, delectus nostrum? Et vel voluptate tenetur perferendis!
-    Laudantium quis consectetur facere exercitationem minima, dolorum iusto, aliquam sint, numquam enim quaerat dolores. Distinctio vero harum voluptatem rem voluptate unde ullam quos et molestias iste, soluta nemo, temporibus omnis!
-    Veniam, quibusdam placeat. Tenetur, eaque sit delectus illo perspiciatis reiciendis laborum assumenda adipisci cupiditate, quae, totam eveniet labore fuga quas enim dolorum? Quidem, quam! Nemo nostrum illum voluptatibus atque optio?
-    Quam, odio quibusdam culpa illum repellendus esse consectetur beatae animi iusto fugiat porro tempora ratione magni quo! Eum earum nobis aspernatur expedita deleniti quo eaque iste similique! Commodi, molestiae assumenda!
-    Fugiat quo nihil, et repudiandae inventore, quidem recusandae molestias est vero quasi libero consectetur voluptatum eaque ut voluptatem enim accusamus doloribus? Ducimus accusantium corporis maiores! Deleniti consequuntur quidem eveniet error?
-    Sequi nihil vero sunt, qui ex porro asperiores incidunt illum vel in facere aut, quia error corrupti debitis, modi earum animi mollitia cumque! Dolor facere vel at nemo voluptatem. Illo.
-    Tempore vero quaerat magni dolorum a vel error, obcaecati dolor iusto? Maxime recusandae ratione saepe adipisci vitae labore accusantium nobis nesciunt voluptatibus optio magni tempore eaque, facilis necessitatibus exercitationem! Atque!
-    Aut sit sed dolorem minus iusto amet cupiditate dicta, ea officia consequatur molestias facilis alias? Saepe animi quisquam atque, molestiae vitae excepturi. Iure ad maxime corporis sed commodi dolores quaerat.
-    Molestiae consectetur dolores, rem saepe officia atque minima dolorem eius, mollitia qui similique sequi esse incidunt laboriosam obcaecati amet facere? Vitae, impedit cumque quasi molestias praesentium repudiandae itaque asperiores voluptas.
-    Sit porro molestiae officia perferendis officiis ratione. Atque accusantium ea tempore animi recusandae quibusdam sit repellat, rerum maiores, facilis mollitia porro. Esse consequuntur ullam molestiae, ad doloremque dicta illo. Incidunt!
-    Dolorem quos earum tempore reprehenderit illum fuga laboriosam. Incidunt ex quo excepturi quia, odit voluptates fugiat quasi expedita eius magni distinctio voluptatibus, eligendi debitis aspernatur impedit obcaecati, quaerat modi aliquid!
-    Praesentium dolor minus, suscipit repellat mollitia autem, dolorem iusto sapiente, magni aut libero quidem deleniti amet ab ipsa. Explicabo quibusdam voluptas vero iusto eum, eaque tempore obcaecati vel minima quod!
-    Velit expedita provident commodi amet quos quis atque, sequi pariatur ullam eius dignissimos earum. Voluptatum, veritatis sint blanditiis, adipisci ipsam perferendis est nostrum repellat, ab accusamus dolores laudantium nam eveniet.</h1>
-  </div>;
+  return (
+    <div className="bg-white px-5 py-5 rounded-lg">
+      <div className="top-10 gap-6 rounded p-4 font-semibold text-[22px]">
+        Overview
+      </div>
+      <div className="px-5">
+        <hr className="w-full"></hr>
+        <div className="flex flex-row justify-between pt-6 pb-3">
+          <div className="text-[#63748A] font-medium w-[40%]">Location</div>
+          <div className="font-semibold w-[40%]">Lagos, Nigeria</div>
+        </div>
+        <hr className="w-full"></hr>
+        <div className="flex flex-row justify-between  pt-6 pb-3">
+          <div className=" text-[#63748A] font-medium w-[40%]">Education</div>
+          <div className="w-[40%]">
+            <h1 className="font-semibold">
+              University Of Ibadan
+              <p className="text-[#63748A]">BSc Economics, 2014-2018</p>
+            </h1>
+            <h1 className="font-semibold pt-4">
+              Federal Govt. College, Ohafia
+              <p className="text-[#63748A]">SSCE, 2008-2018</p>
+            </h1>
+          </div>
+        </div>
+        <hr className="w-full"></hr>
+        <div className="flex flex-row justify-between pt-6 pb-3">
+          <div className="text-[#63748A] font-medium w-[40%]">Work Experience</div>
+          <div className="w-[40%]">
+            <h1 className="font-semibold">
+              Product Lead
+              <p className="text-[#63748A]">Smuv Legacy Limited 2014-2018</p>
+            </h1>
+            <h1 className="font-semibold pt-4">
+              Creative Director
+              <p className="text-[#63748A]">MTN, 2008-2013</p>
+            </h1>
+            <h1 className="font-semibold pt-4">
+              Art Director
+              <p className="text-[#63748A]">Globalcom, 2005-2008</p>
+            </h1>
+          </div>
+        </div>
+        <hr className="w-full"></hr>
+        <div className="flex flex-row justify-between pt-6 pb-3">
+          <div className="text-[#63748A] font-medium w-[40%]">Skills</div>
+          <div className="break-normal font-semibold w-[40%]">
+            Strategy, Product Magement, Unit Economics UX Design
+          </div>
+        </div>
+        <hr className="w-full"></hr>
+        <div className="flex flex-row justify-between pt-6 pb-3">
+          <div className="text-[#63748A] font-medium w-[40%]">Years Of Experience</div>
+          <div className="font-semibold w-[40%]">11</div>
+        </div>
+        <hr className="w-full"></hr>
+        <div className="flex flex-row justify-between pt-6 pb-3">
+          <div className="text-[#63748A] font-medium w-[40%]">
+            Preferred Employer Size
+          </div>
+          <div className="font-semibold w-[40%]">Startups, Middle Business</div>
+        </div>
+        <hr className="w-full"></hr>
+        <div className="flex flex-row justify-between pt-6 pb-3">
+          <div className="text-[#63748A] font-medium w-[40%]">Languages</div>
+          <div className="font-semibold w-[40%]">English, French, Yoruba</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Overview;
